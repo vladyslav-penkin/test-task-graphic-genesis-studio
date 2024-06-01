e43125a930fad415aad7437ddeb61201ab95bbe7
Vladyslav Penkin <vy891078@gmail.com>
2024-06-01 20:33:38 +0300

    changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,19 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
       {
         test: /\.scss$/,
         use: [
@@ -17,5 +28,10 @@ module.exports = {
         ]
       }
     ]
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
   }
 };
